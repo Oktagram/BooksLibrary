acd1d518ee325e130efa8f60b9a6708374e18954
Anton Halko <ahalko@modest.nl>
2020-10-05 22:04:07 +0300

    Define Books controller endpoints

---
+++ BooksLibrary.API/BooksLibrary.API/Controllers/BooksController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksLibrary.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet("/books")]
+        public async Task<IActionResult> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("/books/borrowed")]
+        public async Task<IActionResult> GetBorrowed()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("/books")]
+        public async Task<IActionResult> AddBook()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("/books/order")]
+        public async Task<IActionResult> OrderBook()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("/books/{id}")]
+        public async Task<IActionResult> RemoveBook()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
